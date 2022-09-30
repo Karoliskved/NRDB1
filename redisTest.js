c5a2bc6f283bc8e574f63d3a83fc674f73890290
TomasKasperavicius <tomkasper01@gmail.com>
2022-09-30 21:48:11 +0300

    add concurrency

--- redisTest.js
+++ redisTest.js
@@ -1,25 +1,60 @@
-import { createClient } from 'redis';
-console.log('test2')
-const client = createClient({
-    url: 'redis://default:redispw@localhost:49153'
-  });
+const redis = require('redis');
+const client = redis.createClient();
 
-client.on('error', (err) => console.log('Redis Client Error', err));
-console.log('test2')
-await client.connect();
-console.log('test3')
-const companyProductKey='company1:produc1'
-const keyValue='8'
-await client.set(companyProductKey, keyValue);
-const value = await client.get(companyProductKey);
-console.log('test5')
-console.log(keyValue)
-client.keys("*")
-async function setProductkey(companyName, productName, keyValue){
-  const key=companyName+":"+productName
-  await client.set(key, keyValue);
-  const value = await client.get(key);
-  console.log(value)
+client.on('error', (err) => console.log('Redis error', err))
+
+async function setProduct(companyName, productName, quantity) {
+  const key = formatKey(companyName, productName)
+  await client.connect()
+  await client.set(key, quantity)
+  client.disconnect()
 }
-console.log("hello")
-setProductkey('test', 'test2', '5')
+
+const formatKey = (companyName, productName) =>{
+    return companyName + ":" + productName
+}
+
+const buyProduct = async (key, clientNumber) => {
+  try {
+      const client = redis.createClient()
+      await client.connect()
+      
+      await client.watch(key)
+      const result = await client.get(key)
+      const productCount = parseInt(result)
+      if (productCount > 0) {
+          const multi = client.multi()
+          multi.set(key, productCount - 1)
+          const results = await multi.exec()
+          if (results.length === 0) {
+              console.log("Client "+ clientNumber + " failed to buy the product with compound key: "+ key)
+          }
+          else {
+              console.log("Client "+ clientNumber + " successfuly bought the product with compound key: "+ key)
+          }
+      }
+      else {
+        console.log("Client "+ clientNumber + " failed to buy the product with compound key: "+ key+" out of stock")
+      }
+      client.disconnect()
+  } catch (error) {
+      if (error instanceof redis.WatchError) {
+          console.log("Transaction failed","Client "+ clientNumber + " failed to buy the product with compound key: "+ key)
+      }else{
+          console.log(error)
+      }
+  }
+}
+const companyName = "company1"
+const productName = "product1"
+const productQuantity = 2
+setProduct(companyName,productName,productQuantity)
+const compoundKey = formatKey(companyName,productName)
+buyProduct(compoundKey, 1)
+setTimeout(()=>{
+  buyProduct(compoundKey, 2)
+},3000)
+buyProduct(compoundKey, 3)
+
+
+
